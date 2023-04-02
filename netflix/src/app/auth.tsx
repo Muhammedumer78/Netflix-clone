2c47df391bbec0e9cf9e18d43d39614e73d6005e
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-02 17:13:53 +0500

    created the sign in element and email area

--- netflix/src/app/auth.tsx
+++ netflix/src/app/auth.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-
+import Input from "../components/input";
 
 
 const Auth = () => {
@@ -11,7 +12,18 @@ const Auth = () => {
         <img src="/images/logo.png" alt="logo">
         </img>
         </nav>
+        <div className="flex justify-center ">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                <h2 className="text-white text-4xl mb-8 font-semibold ">
+                   Sign In 
+                </h2>
+            <div className="flex flex-col gap-4">
+            <Input/>
             </div>
+            </div>
+
+        </div>
+        </div>
         </div>
     );
     }
